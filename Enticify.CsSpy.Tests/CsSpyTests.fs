82de1717414667a936a8169ab9e47b22a6c82a7e
Ben Taylor <ben.taylor.uk@gmail.com>
2012-08-17 12:12:21 +0100

    Initial commit with bare bones working DictionaryClass Visualizer for VS 2010 only.  Not much to see yet.

---
+++ Enticify.CsSpy.Tests/CsSpyTests.fs
@@ -0,0 +1,18 @@
+﻿namespace Enticify.CsSpy.Tests
+
+module CsSpyTests =
+    open Microsoft.VisualStudio.DebuggerVisualizers
+    open Xunit
+    open Microsoft.CommerceServer.Runtime
+    open Enticify.CsSpy.DictionaryVisualizer
+
+    [<Fact>]
+    let ``runs ok`` () =
+        let dic = DictionaryClass()
+        dic.["_name"] <- "ben"
+        dic.["_iamnull"] <- null  
+        dic.["_iamdbnull"] <- System.DBNull.Value 
+        let vizHost = VisualizerDevelopmentHost (dic, typeof<DictionaryVisualizerDialog>, typeof<DictionaryObjectSource>)
+        vizHost.ShowVisualizer()
+        ()
+
---
+++ Enticify.CsSpy/CsSpy.fs
@@ -0,0 +1,45 @@
+﻿namespace Enticify.CsSpy
+
+module DictionaryVisualizer =
+    open Microsoft.VisualStudio.DebuggerVisualizers
+    open System.Windows.Forms
+    open System.Diagnostics
+    open Microsoft.CommerceServer.Runtime
+    open System.Runtime.Serialization.Formatters.Binary
+
+    type DictionaryObjectSource() = 
+        inherit VisualizerObjectSource() 
+        override this.GetData(target : obj, outgoingData : System.IO.Stream) = 
+            let formatter = BinaryFormatter() 
+            match target with
+            | :? DictionaryClass as dic -> 
+                let data = 
+                    [   for key in dic do
+                            match key with
+                            | :? string as s -> 
+                                let value = 
+                                    match dic.[s] with
+                                    | null -> "null"
+                                    | value -> value.ToString() 
+                                yield (s, value) 
+                            | _ -> ()//Should never happen! 
+                    ]
+                formatter.Serialize(outgoingData, data) 
+            | _ -> formatter.Serialize(outgoingData, "No data.")
+            ()
+
+    type DictionaryVisualizerDialog() =
+        inherit DialogDebuggerVisualizer()
+        override x.Show(dvs:IDialogVisualizerService, vop:IVisualizerObjectProvider) = 
+            let show msg = MessageBox.Show(msg) |> ignore 
+            match vop.GetObject() with 
+            | :? string as s -> show s 
+            | :? ((string*string) list) as sl -> show (sl |> Seq.fold (fun acc (key,value) -> acc + " " + key + ":" + value) "")
+            | _ -> show "Unexpected object provided"
+            ()
+
+    [<assembly:DebuggerVisualizer(typeof<DictionaryVisualizerDialog>, 
+                                  typeof<DictionaryObjectSource>, 
+                                  Target = typedefof<DictionaryClass>, 
+                                  Description = "Enticify CsSpy Dicitonary Visualizer")>]
+    do()
